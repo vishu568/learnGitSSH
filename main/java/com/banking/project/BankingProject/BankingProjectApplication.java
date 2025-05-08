ec38712304ebd25118066af79b7f1ca319b17e8a
vishu568 <vishu.568@gmail.com>
2025-05-08 15:49:22 +0530

    file modified

---
+++ main/java/com/banking/project/BankingProject/BankingProjectApplication.java
@@ -0,0 +1,17 @@
+package com.banking.project.BankingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BankingProjectApplication.class, args);
+		
+		//SpringBootApplication Starterq
+		System.Out.println("HelloWorld")
+	}
+
+}
